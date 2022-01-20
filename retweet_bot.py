985000cd2fa42815fff31e0993d0552a1879852a
Sean Folan <75865821+Sfolan2@users.noreply.github.com>
2022-01-19 19:10:01 -0500

    Update retweet_bot.py

--- retweet_bot.py
+++ retweet_bot.py
@@ -10,7 +10,7 @@ consumer_secret=config.API_Secret,
 access_token=config.Access_Token,
 access_token_secret=config.Access_Secret)
 
-query='Impact -is:retweet' 
+query='from:briancox03 (@Stupid_TweetBot OR -is:reply) -is:retweet' 
 
 #Get Last Tweet
 lastTweet=datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")#datetime.datetime.utcnow().replace(microsecond=1).isoformat()
@@ -18,15 +18,13 @@ print(lastTweet)
 
 #Start Loop
 while True:
+    print("Waiting 15 minutes")
     time.sleep(900)
     #Get every tweet since last tweet
-    response=client.search_recent_tweets(query=query,max_results=10, start_time=lastTweet)
+    response=client.search_recent_tweets(query=query,max_results=100, start_time=lastTweet)
     if response.data:
         for resp in response.data:
-            print(";ebron")
+            print("tweeted")
             client.retweet(resp.id)
             time.sleep(60)
     lastTweet=datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")#datetime.datetime.utcnow().replace(microsecond=1).isoformat()
-    
-
-#Working Version
